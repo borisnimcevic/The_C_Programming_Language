1e435bf1a1f8cb0bc15fe12e60a6001ab0ead378
borisnimcevic <borisnimcevic@gmail.com>
2018-06-13 00:36:22 -0700

    first chapter add

---
+++ ch1_A_Tutorial_Introduction/Exercise_1-1/e1-1_hello_world.c
@@ -0,0 +1,12 @@
+/*
+Made by borisnotes on Jun 9th, 2018
+From book "The C Programming Language" by Brain W. Kerninghan and Dennis M. Ritchie
+Exercise 1.1
+*/
+
+#include <stdio.h>
+
+int main(){
+
+	printf("hello world\n");
+}
