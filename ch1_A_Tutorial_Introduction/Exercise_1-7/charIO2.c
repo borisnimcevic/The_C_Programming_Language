5a06881ce2a872bfc0f2c717e4be17fcb482d5fb
borisnimcevic <borisnimcevic@gmail.com>
2018-06-24 12:59:07 -0700

    Solution for the exercise 1.7

---
+++ ch1_A_Tutorial_Introduction/Exercise_1-7/charIO2.c
@@ -0,0 +1,19 @@
+/*
+   Made by borisnotes on Jun 24rd, 2018
+   From book "The C Programming Language" by Brain W. Kerninghan and Dennis M. Ritchie
+
+   Exercise 1.7
+   Write a program to print the value of EOF.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+int main(){
+
+	printf("The value of EOF is %d\n", EOF);
+	return EXIT_SUCCESS;
+
+}
